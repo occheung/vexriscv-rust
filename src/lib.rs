6008a2dc0409428b7a44bd5776ca5264002746ad
occheung <dc@m-labs.hk>
2021-07-08 16:43:36 +0800

    lib: support feature llvm_asm

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 //! Low level access to VexRiscv RISC-V cores
 
 #![no_std]
-#![cfg_attr(feature = "inline-asm", feature(asm))]
+#![cfg_attr(feature = "inline-asm", feature(llvm_asm))]
 
 pub mod register;
